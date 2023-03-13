005540368c21e7bef4107e3049c158283c6e0271
Corey N <coreynotice@yahoo.com>
2023-03-13 11:55:54 -0400

    basic nav with css

---
+++ src/components/Basket.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Basket(props) {
+  return (
+    <div>Basket</div>
+  )
+}
+
+export default Basket
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header(props) {
+  return (
+    <header className='row block center'>
+        <div>
+            <a href='#/'>
+
+        <h1>Small Shopping Cart</h1>
+        </a>
+        </div>
+        <div>
+            <a href='#/cart'>Cart</a><a href='#/cart'>Sign In</a>
+        </div>
+    </header>
+  )
+}
+
+export default Header
