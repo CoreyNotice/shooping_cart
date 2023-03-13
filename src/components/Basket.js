8c72f9a648d55fdb1c3a45531e656da3592ae642
Corey N <coreynotice@yahoo.com>
2023-03-13 15:59:11 -0400

    adding basket works

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
 function Basket(props) {
+  const{cartItems,onAdd,onRemove}=props
   return  <aside className='block col-1'>
   <h2>Cart Items</h2>
+  <div>{cartItems.length===0&&<div>Cart is Empty</div>}</div>
+ {cartItems.map((item)=>(
+  <div key={item.id} className='row'>
+    <div className='col-2'>{item.name}</div>
+    <div className='col-2'>
+      <button onClick={()=>onAdd(item)} className='add'>
+        +
+      </button>
+      <button onClick={()=>onAdd(item)} className='add'>
+        -
+      </button>
+    </div>
+    <div className='col-2 text-right'>
+      {item.qty}*${item.price.toFixed(2)}
+    </div>
+  </div>
+ ))}
   </aside>
   
 }
